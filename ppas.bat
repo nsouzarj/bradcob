2cd16e8a18a9b27d61a8a3e234d021499291a21c
Nelson Seixas de Souza <nsouzarj@outlook.com>
2023-10-13 06:31:29 -0300

    Alteracao basica

---
+++ ppas.bat
@@ -0,0 +1,12 @@
+@echo off
+SET THEFILE=C:\bradcob\menu.exe
+echo Linking %THEFILE%
+C:\lazarus\fpc\3.2.0\bin\x86_64-win64\ld.exe -b pei-x86-64  --gc-sections   --subsystem windows --entry=_WinMainCRTStartup    -o C:\bradcob\menu.exe C:\bradcob\link.res
+if errorlevel 1 goto linkend
+goto end
+:asmend
+echo An error occurred while assembling %THEFILE%
+goto end
+:linkend
+echo An error occurred while linking %THEFILE%
+:end
